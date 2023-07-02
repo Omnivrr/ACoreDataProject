de77f7c2821ce91178a53fa34d752616f0acff7b
Omnivrr <Omnivrr@gmail.com>
2023-07-02 10:01:15 +0100

    Initial Commit

---
+++ CoreDataProject/CoreDataProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataProjectApp.swift
+//  CoreDataProject
+//
+//  Created by Bukhari Sani on 02/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
